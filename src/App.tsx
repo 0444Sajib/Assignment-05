1ebc05aadfb67a827a503d7e725db924ab9dcffd
0444Sajib <sajibkst0666@gmail.com>
2026-09-10 20:53:03 +0600

    first connecting to assignment-05

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      
+
+    <h2 className="text-5xl text-red-500">Hello Projects</h2>
+    <button className="btn btn-success">Success</button>
+    <button className="btn btn-success">Success</button>
+
+
+
+
+   </>
+  )
+}
+
+export default App
