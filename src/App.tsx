cedd60f6f755de3ab12da47fbd48e5ed6569d15b
0444Sajib <sajibkst0666@gmail.com>
2026-09-13 11:33:12 +0600

    Completed our Hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hero />
 
       
 
